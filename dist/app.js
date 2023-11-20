f6b8667fb4be13fa2f4e15eeb35a0af9d2222faf
soriful15 <sorifulislam.rs10@gmailcomm>
2023-11-21 03:38:33 +0600

    overall ok

--- dist/app.js
+++ dist/app.js
@@ -9,14 +9,75 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get('/', (req, res) => {
-    res.send('Hello Md soriful Islam World!!!');
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+// app.use('/', userRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/course', courseRouter);
+// userRouter.get('/api/v1/users/create-user', (req: Request, res: Response) => {
+userRouter.post('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "user is crated successfully",
+        data: user
+    });
 });
-app.post('/', (req, res) => {
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "course is crated successfully",
+        data: course
+    });
+});
+// middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+/* // app.get('/:userId/:subId', (req:Request, res:Response) => { //params er jonno
+app.get('/', logger, (req: Request, res: Response) => { //query er jonno
+  // console.log(req.params) //params er jonno
+
+  console.log(req.query) //query er jonno
+  console.log(req.query.name)
+  res.send('Hello Md soriful Islam World!!!')
+}) */
+// error
+app.get('/', logger, (req, res, next) => {
+    try {
+        // res.send(some)
+        res.send("something");
+    }
+    catch (error) {
+        next(error);
+    }
+});
+app.post('/', logger, (req, res) => {
     console.log(req.body);
     // res.send('got data')
     res.json({
         message: "successfully received data"
     });
+});
+// aita sobar last a use korte hoibo
+app.all('*', (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: "Route not found"
+    });
+});
+// global error handle
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: "something wrong  data"
+        });
+    }
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,110 @@
 // const express = require('express')
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 
 
 // parsers
 app.use(express.json());
-app.use(express.text())
+app.use(express.text());
+
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+
+// app.use('/', userRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/course', courseRouter);
+
+
+// userRouter.get('/api/v1/users/create-user', (req: Request, res: Response) => {
+userRouter.post('/create-user', (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "user is crated successfully",
+    data: user
+  })
+
+});
+courseRouter.post('/create-course', (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course is crated successfully",
+    data: course
+  })
+
+});
 
 
 
-app.get('/', (req:Request, res:Response) => {
+
+
+
+
+
+// middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname)
+  next()
+}
+
+
+/* // app.get('/:userId/:subId', (req:Request, res:Response) => { //params er jonno
+app.get('/', logger, (req: Request, res: Response) => { //query er jonno
+  // console.log(req.params) //params er jonno
+
+  console.log(req.query) //query er jonno
+  console.log(req.query.name)
   res.send('Hello Md soriful Islam World!!!')
+}) */
+
+
+// error
+app.get('/', logger, (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // res.send(some)
+    res.send("something")
+  }
+  catch (error) {
+    next(error)
+
+  }
+
 })
 
-app.post('/', (req:Request,res:Response)=>{
-console.log(req.body)
-// res.send('got data')
-res.json({
-  message:"successfully received data"
+app.post('/', logger, (req: Request, res: Response) => {
+  console.log(req.body)
+  // res.send('got data')
+  res.json({
+    message: "successfully received data"
+  })
 })
+
+
+// aita sobar last a use korte hoibo
+app.all('*',(req:Request, res:Response)=>{
+  res.status(400).json({
+    success:false,
+    message:"Route not found"
+  })
 })
+
+// global error handle
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+
+  if (error) {
+    console.log(error)
+    res.status(400).json({
+      success: false,
+      message: "something wrong  data"
+    })
+  }
+
+})
+
 
 export default app;
